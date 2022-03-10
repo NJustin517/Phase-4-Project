724411b94f2f4fe50bacabc719a913c1fe444553
Justin Luciano <justinlluciano@gmail.com>
2022-03-10 09:10:27 -0800

    cart layout done

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import CartCard from "./CartCard"
+import CartCard from "./CartCard";
 
 function Cart({ user }) {
   const [cart, setCart] = useState(null);
@@ -9,14 +9,26 @@ function Cart({ user }) {
       .then((resp) => resp.json())
       .then((receivedItems) => setCart(receivedItems.cart));
   }, []);
-  if ( cart === null ) {
-    return <h2>Loading...</h2>
-  } 
+  if (cart === null) {
+    return <h2>Loading...</h2>;
+  }
 
   const cards = cart.map((item) => {
-    return <CartCard key={item.id} item={item}/>
-  })
-  return (<> {cards} </> )
+    return <CartCard key={item.id} item={item} />;
+  });
+  return (
+    <div className="cart">
+      <h1 className="cartTitle">CART</h1>
+      {cards}
+      <hr className="totalLine"></hr>
+      <span>
+        <b className="cartTotal">Total:</b>
+        <b className="cartTotal" style={{ marginLeft: "75%" }}>
+          ${user.cart_sum}.00
+        </b>
+      </span>
+    </div>
+  );
 }
 
 export default Cart;
--- client/src/components/CartCard.js
+++ client/src/components/CartCard.js
@@ -1,27 +1,59 @@
-
-
 function CartCard({ item }) {
+  // function handleDeleteClick() {
+  //   fetch(`/carts/${cart.id}`, {
+  //     method: "DELETE",
+  //   }).then((res) => {
+  //     if (res.ok) {
+  //       console.log(res);
+  //     } else {
+  //       res.json().then(console.log);
+  //     }
+  //   });
+  // }
 
-return ( <> 
-    <div class="col s12 m7">
+  return (
+    <>
+      <div id="container">
+        <div class="product-details">
+          <h1>{item.name}</h1>
 
-      <div class="card horizontal">
-        <div class="card-image">
-          <img src={item.image_url} class="fadeIn cardImage"></img>
-        </div>
-        <div class="card-stacked">
-          <div class="card-content">
-            <span class="card-title">
-                <b>{item.name}</b>
-                <b style={{marginLeft: "20px"}}>${item.price}.00</b>
-            </span>
+          <p class="information">{item.description}</p>
 
-            <p>{item.description}</p>
+          <div class="control">
+            <button class="btn">
+              <span class="price">${item.price}.00</span>
+              <span class="shopping-cart">
+                <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+              </span>
+              <span class="buy">Remove</span>
+            </button>
           </div>
         </div>
-      </div>
-    </div>
-  </>
-)}
 
-export default CartCard
+        <div class="product-image">
+          <img src={item.image_url} alt=""></img>
+
+          {/* <div class="info">
+            <h2> Description</h2>
+            <ul>
+              <li>
+                <strong>Height : </strong>5 Ft{" "}
+              </li>
+              <li>
+                <strong>Shade : </strong>Olive green
+              </li>
+              <li>
+                <strong>Decoration: </strong>balls and bells
+              </li>
+              <li>
+                <strong>Material: </strong>Eco-Friendly
+              </li>
+            </ul>
+          </div> */}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default CartCard;
